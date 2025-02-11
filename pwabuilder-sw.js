cca0fc8b88bdc4ac23aa7f34c1f5c4e61221b4dc
yakovchikivan3 <144143810+yakovchikivan3@users.noreply.github.com>
2025-02-11 20:17:37 +0200

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,25 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "index.html";
+const CACHE_NAME = "app-cache-v1";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+if (workbox) {
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+  workbox.routing.registerRoute(
+    ({ request }) => true, // Кешировать все запросы
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: CACHE_NAME,
+    })
+  );
+
+  self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+      self.skipWaiting();
+    }
+  });
+} else {
+  console.error("Workbox не загрузился");
+}
